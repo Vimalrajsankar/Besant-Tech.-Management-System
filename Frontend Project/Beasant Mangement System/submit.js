c41f6e0cecfa4c1cd66bbe59fe6da3a6fbd3f7b5
Vimalrajsankar <111079482+Vimalrajsankar@users.noreply.github.com>
2022-12-01 23:04:01 +0530

    Add files via upload

---
+++ Frontend Project/Beasant Mangement System/submit.js
@@ -0,0 +1,25 @@
+const enquairybtn=document.getElementById("enquairy")
+function over(){
+   
+    enquairybtn.style.display="block"
+}
+function out(){
+   
+    enquairybtn.style.display="none"
+}
+
+const applybtn=document.getElementById("apply")
+function myfunction1(){
+    applybtn.style.display="block"
+}
+function myfunction2(){
+    applybtn.style.display="none"
+}
+// // let x=document.getElementById('btn')
+// // x.addEventListener('click',myFunction)
+// function myFunction(){
+// location.href="submit.html"
+// // console.log("hi")
+// }
+
+
